5690ce2fad471fc10971f35ea94f6e59412376bc
hardevs1387 <63764366+hardevs1387@users.noreply.github.com>
2022-11-02 12:10:53 +1000

    the source code for the application

---
+++ The application/app/src/main/java/com/example/draft1/ui/home/DetailsFragment.java
@@ -0,0 +1,61 @@
+package com.example.draft1.ui.home;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.draft1.databinding.FragmentDetailsBinding;
+import com.example.draft1.databinding.FragmentMapsBinding;
+import com.example.draft1.ui.settings.HelpFragment;
+
+public class DetailsFragment extends Fragment {
+
+    private FragmentDetailsBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(DetailsFragment.this).popBackStack();
+            }
+        });
+
+        binding.img001.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://virtual-tours.com.au/federation/brisbane/";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
